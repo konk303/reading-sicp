df4697862b5538f7db8eaa821e202135f3147b4b
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2015-11-02 20:26:56 +0900

    another way of 2.28

--- 2/2.28.scm
+++ 2/2.28.scm
@@ -1,9 +1,19 @@
 (define (fringe x)
-  (cond ((null? x) '())
+  (cond ((null? x) (list))
         ((not (pair? x)) (list x))
         (else
          (append (fringe (car x))
                  (fringe (cdr x))))))
+
+
+(define (fringe2 x)
+  (define (iter a result)
+    (if (null? a)
+        result
+        (if (pair? a)
+            (iter (car a) (iter (cdr a) result))
+            (cons a result))))
+  (iter x null))
 
 
 (define x (list (list 1 2) (list 3 4)))
@@ -12,4 +22,12 @@
 (newline)
 
 (display (fringe (list x x)))
+;; (1 2 3 4 1 2 3 4)
+
+
+(display (fringe2 x))
+;; (1 2 3 4)
+(newline)
+
+(display (fringe2 (list x x)))
 ;; (1 2 3 4 1 2 3 4)
