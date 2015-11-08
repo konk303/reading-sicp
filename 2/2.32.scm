805c78fbcf82662310bf7bb33bd9979500cd688d
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2015-11-08 13:37:48 +0900

    2.32

--- 2/2.32.scm
+++ 2/2.32.scm
@@ -1,8 +1,11 @@
+;; 1. restが、cdrのsubsetとする
+;; 1. cdrのsubsetと、carとrestのpairの合計がlistのsbuset
+
 (define (subsets s)
   (if (null? s)
-      (list nil)
+      (list null)
       (let ((rest (subsets (cdr s))))
-        (append rest (map ⟨??⟩ rest)))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
 
-(subesets (1 2 3))
+(display (subsets '(1 2 3)))
 ;; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
