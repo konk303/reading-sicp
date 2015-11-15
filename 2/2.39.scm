8a2862d038ad16837d149697e5c7eb556f1932ce
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2015-11-15 18:56:03 +0900

    2.39

---
+++ 2/2.39.scm
@@ -0,0 +1,30 @@
+(define nil '())
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+
+(define (reverse-r sequence)
+  (fold-right
+   (lambda (x y) (append y (cons x nil))) nil sequence))
+
+(display (reverse-r '(1 2 3 4)))
+(newline)
+
+(define (reverse-l sequence)
+  (fold-left
+   (lambda (x y) (cons y x)) nil sequence))
+
+(display (reverse-l '(1 2 3 4)))
+(newline)
