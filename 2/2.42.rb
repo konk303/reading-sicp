e798760ad4b52051985771a741dfe83c9da04169
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2015-12-02 02:26:14 +0900

    2.42

--- 2/2.42.rb
+++ 2/2.42.rb
@@ -1,10 +1,12 @@
 class Array
   def flatmap
-    map { |l| l.map { |ll| yield ll } }.flatten
+    map { |l| yield l }.flatten 1
   end
 end
 
 class Queens
+  Position = Struct.new :col, :row
+
   attr_accessor :board_size
 
   def initialize(board_size)
@@ -16,17 +18,36 @@ class Queens
     if k == 0
       [empty_board]
     else
-      queen_cols(k - 1).flatmap do |rest_of_queens|
-        1.upto(board_size).map { |new_row| p new_row; adjoin_position new_row, k, rest_of_queens }
-      end
+      queen_cols(k - 1).flatmap { |rest_of_queens|
+        1.upto(board_size).map { |new_row| adjoin_position new_row, k, rest_of_queens }
+      }.select { |positions| safe? k, positions }
     end
   end
 
   def empty_board
-    []
+    nil
   end
 
   def adjoin_position(row, col, roq)
-    roq.dup << [col, row]
+    rests = case roq
+            when nil
+              []
+            when Array
+              roq.dup
+            else
+              [roq]
+            end
+    rests << Position.new(col, row)
+  end
+
+  def safe?(k, positions)
+    rests = positions.dup
+    current = rests.pop
+    rests.all? do |rest|
+      diff = current.col - rest.col
+      rest.row != current.row &&
+        rest.row != (current.row - diff) &&
+        rest.row != (current.row + diff)
+    end
   end
 end
