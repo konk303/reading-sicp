a7fb70620b95239253a5871a8d0b36a51860cbfb
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2015-12-15 20:04:21 +0900

    2.45

--- 2/2.45.scm
+++ 2/2.45.scm
@@ -13,3 +13,15 @@
   (let ((combine4 (square-of-four flip-horiz identity
                                   rotate180 flip-vert)))
     (combine4 (corner-split painter n))))
+
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(define (split first-func, second-func)
+  (define (ret painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (ret painter (- n 1))))
+          (first-func painter (second-func smaller smaller)))))
+  ret)
