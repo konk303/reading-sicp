d1305c7cd796e93436b46581a8c6747c36b12085
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2015-12-21 19:46:30 +0900

    2.47

---
+++ 2/2.47.scm
@@ -0,0 +1,20 @@
+;; 1. in list
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+(define (edge1-frame frame)
+  (car (cdr frame)))
+(define (edge2-frame frame)
+  (car (cdr (cdr frame))))
+
+;; 1. in pair
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define (origin-frame frame)
+  (car frame))
+(define (edge1-frame frame)
+  (car (cdr frame)))
+(define (edge2-frame frame)
+  (cdr (cdr frame)))
